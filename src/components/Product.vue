c2f0b843b4510742036992af635341e9b1ed92db
ChantalRamirez <ramirez_yessica@ucol.mx>
2023-11-21 14:28:47 -0600

    Components in proyects

---
+++ src/components/Product.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="col-2 m-auto"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-createApp(App).use(router).mount("#app");
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+//import 'bootstrap/dist/css/bootstrap.css'
+
+library.add(faShoppingCart)
+
+createApp(App).use(router).component('fa', FontAwesomeIcon).mount('#app')
